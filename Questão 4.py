7e2cb3579d85a54a82e95fd63082c196f261cb5c
Sandra Piola <guilhermeap02@gmail.com>
2024-03-24 15:49:50 -0300

    questão 4 finalizada

---
+++ Questão 4.py
@@ -0,0 +1,7 @@
+# 4. Leia um número real e imprima o resultado do quadrado desse número.
+
+real = int(input("Digite um numero real "))
+operacao = (real * real)
+
+print("o quadrado do numero",(operacao))
+
