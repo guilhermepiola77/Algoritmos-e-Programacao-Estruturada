3430dfe64b956d89b6a732605f0cf3b555ee69c2
Sandra Piola <guilhermeap02@gmail.com>
2024-03-24 16:09:24 -0300

    Questao 5 finalizada

---
+++ Questão 5.py
@@ -0,0 +1,7 @@
+# 5. Leia um número real e imprima a quinta parte deste número
+
+real = int(input("Digite um numero real "))
+
+operacao = (real / 5)
+
+print("a quinta parte do numero é",(operacao))
