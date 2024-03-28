da09e6c7d60ce9679f8b373d0db6618c99d4330a
Sandra Piola <guilhermeap02@gmail.com>
2024-03-28 16:42:31 -0300

    Questão 8 Finalizada

---
+++ Questão 8.py
@@ -0,0 +1,8 @@
+# 8. Leia uma temperatura em graus Kelvin e apresente-a convertida em graus Celsius. A
+# fórmula de conversão é: 𝐶 = 𝐾 − 273.15, sendo 𝐶 a temperatura em Celsius e 𝐾 a
+# temperatura em Kelvin.
+
+Kelvin = float(input("Informe a temperatura em Celsius:"))
+Celsius = (Kelvin - 273,15)
+print("A temperatura em celsius é:",Celsius)
+
