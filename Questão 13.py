b03df6f07d9e395bc12f18042fea6769cebb8132
Sandra Piola <guilhermeap02@gmail.com>
2024-03-28 17:06:38 -0300

    Questão 13 Finalizada

---
+++ Questão 13.py
@@ -0,0 +1,9 @@
+# 13. Leia uma distância em quilômetros e apresente-a convertida em milhas. A fórmula de
+# conversão é: 𝑀 =
+# 𝐾
+# 1.61
+# , sendo K a distância em quilômetros e M em milhas.
+
+K = float(input("Informe a velocidade em quilometros:"))
+M = (K / 1.61)
+print("A velocidade em milhas é:",M)
