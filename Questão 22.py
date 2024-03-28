c8b6301a921a3005ddad58dd5ec3e3fc10adebd5
Sandra Piola <guilhermeap02@gmail.com>
2024-03-28 19:36:53 -0300

    Questão 22 Finalizada

---
+++ Questão 22.py
@@ -0,0 +1,9 @@
+# 22. Leia o tamanho do lado de um quadrado e imprima como resultado a sua área.
+
+lado = float(input("Digite o tamanho do lado do quadrado: "))
+
+area = lado * lado 
+area = lado 
+
+print("A área do quadrado é: ",area)
+
