f787f4f3579c5f47b7859c5ef9cc80d0d8e94182
Sandra Piola <guilhermeap02@gmail.com>
2024-03-28 16:58:30 -0300

    Questão 11 Finalizada

---
+++ Questão 11.py
@@ -0,0 +1,9 @@
+# 11. Leia uma velocidade em m/s (metros por segundo) e apresente-a convertida em
+# km/h (quilômetros por hora). A fórmula de conversão é: 𝐾 = 𝑀 ∗ 3.6, sendo 𝐾 a
+# velocidade em km/h e 𝑀 em m/s.
+
+M = float(input("Informe a velocidade em m/s:"))
+K = (M * 3.6)
+print("A velocidade em km/h é:",K)
+
+
