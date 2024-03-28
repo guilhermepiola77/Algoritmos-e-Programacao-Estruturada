dd982e8627316a14270be0f3a4359161981a2b7e
Sandra Piola <guilhermeap02@gmail.com>
2024-03-28 16:16:18 -0300

    Questão 6 Finalizada

---
+++ Questão 6.py
@@ -0,0 +1,11 @@
+# 6. Leia uma temperatura em graus Celsius e apresente-a convertida em graus Fahrenheit.
+# A fórmula de conversão é: 𝐹 = 𝐶 ∗ (
+# 9.0
+# 5.0
+# ) + 32.0, sendo que 𝐹 a temperatura em
+# Fahrenheit e 𝐶 a temperatura em Celsius.
+
+Celsius =float(input("Informe a temperatura em Fahrenheit:"))
+Fahrenheit = (Celsius) * 9/5 + 32  
+print ("A temperatura em Fahrenheit é:",Fahrenheit)
+
