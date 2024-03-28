d98b111f0fbf68d69fa50e6f4ed83d41f2ef55dd
Sandra Piola <guilhermeap02@gmail.com>
2024-03-28 19:53:45 -0300

    Questão 23 Finalizada

---
+++ Questão 23.py
@@ -0,0 +1,10 @@
+# 23. Leia o valor do raio de um círculo e calcule e imprima a área do circulo
+# correspondente. A área do círculo é 𝜋 ∗ 𝑟𝑎𝑖𝑜², considere 𝜋 = 3.141592.
+
+raio = float(input("Digite o valor do raio do circulo: "))
+
+area = 3.141592 * (raio ** 2) 
+
+print("A área do circulo é: ",area)
+
+
