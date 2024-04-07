c50d02b1ba8324fe50cf7b345792936fdb00fe6d
Sandra Piola <guilhermeap02@gmail.com>
2024-04-07 17:00:37 -0300

    Questão 20 Finalizada

--- Aula 3/Questão 20.py
+++ Aula 3/Questão 20.py
@@ -1,2 +1,9 @@
 # 20. Leia um número inteiro e imprima o seu antecessor e o seu sucessor.
 
+numero_inteiro = int(input("Digite um numero inteiro: "))
+
+antecessor = numero_inteiro -1
+sucessor = numero_inteiro + 1
+
+print("O antecessor do numero inteiro é: ",antecessor)
+print("O sucessor do numero inteiro é: ",sucessor)
