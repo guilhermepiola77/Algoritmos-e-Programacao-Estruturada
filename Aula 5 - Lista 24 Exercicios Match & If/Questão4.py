a7c43b088cfd312cc1642c3da931282014938bcf
Sandra Piola <guilhermeap02@gmail.com>
2024-04-11 21:22:38 -0300

    Questão 4 Finalizada

---
+++ Aula 5 - Lista 24 Exercicios Match & If/Questão4.py
@@ -0,0 +1,23 @@
+# 4. Faça um programa que leia um número e, caso ele seja positivo, calcule e mostre:
+# a. O número digitado ao quadrado
+# b. A raiz quadrada do número digitado
+
+import math
+
+def calculadora_numero_positivo(numero):
+    if numero >0:
+
+        quadrado = numero ** 2
+        print("O quadrado do número digitado é:", quadrado)
+
+        raiz_quadrada = math.sqrt(numero)
+        print("A raiz quadrada do número digitado é:", raiz_quadrada)
+
+    else:
+        print("O número digitado não é positivo.")
+
+numero = float(input("Digite um número: "))
+
+calculadora_numero_positivo(numero)
+
+
